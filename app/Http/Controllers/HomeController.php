92f1ef001024174114695e5629c58d3991e09acf
hernansaavedrag <hernan.saavedra@gmail.com>
2024-04-26 11:43:00 -0400

    creando paginas

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    public function index(){
+        //dd('hola mundo'); //dump and die
+        return view('home.index');
+    }
+
+    public function login(){
+        return view('home.login');
+    }
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Mi primer laravel.</title>
+</head>
+<body>
+    <h2>Página de Bienvenida</h2>
+    <h4>Mi primera página de Laravel</h4>
+</body>
+</html>
